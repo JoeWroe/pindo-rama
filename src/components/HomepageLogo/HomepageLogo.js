fd81750f6344b469b427bd4dd1eb7f9881b08f65
Joe Wroe <joe_wroe@icloud.com>
2018-11-25 00:46:22 +0000

    Setup HomepageLogo component

---
+++ src/components/HomepageLogo/HomepageLogo.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class HomepageLogo extends Component {
+    state = {
+        loaded: true
+    }
+
+    render() {
+        return (
+            <div className="HomepageLogo">
+                Pindo Rama
+            </div>
+        );
+    }
+}
+
+export default HomepageLogo;
---
+++ src/components/HomepageLogo/HomepageLogo.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {shallow} from "enzyme";
+
+import HomepageLogo from './HomepageLogo.js'
+
+
+describe('The Homepage Logo Component', () => {
+    it('renders OK', () => {
+        const homepageLogoComponent = shallow(<HomepageLogo />)
+        expect(homepageLogoComponent.state('loaded')).toEqual(true)
+    });
+})
