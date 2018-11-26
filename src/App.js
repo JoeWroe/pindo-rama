c116d50b98fc07c1a8e8507c2c97e875aa265e66
Joe Wroe <joe_wroe@icloud.com>
2018-11-26 23:10:33 +0000

    Add HomepageTitle to page
    Joe

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 
 import HomepageLogo from "./components/HomepageLogo/HomepageLogo";
+import HomepageTitle from "./components/HomepageTitle/HomepageTitle";
 
 class App extends Component {
   render() {
     return (
       <body className="App">
         <HomepageLogo />
+        <HomepageTitle />
       </body>
     );
   }
