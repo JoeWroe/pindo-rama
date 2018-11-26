e6623f2a62dabce40891bfac622840ae264c371c
Joe Wroe <joe_wroe@icloud.com>
2018-11-26 23:08:17 +0000

    Add welcome message to HomepageTitle
    Joe

--- src/components/HomepageTitle/HomepageTitle.js
+++ src/components/HomepageTitle/HomepageTitle.js
@@ -5,7 +5,7 @@ class HomepageTitle extends Component {
     render() {
         return (
             <div className='homepageTitle'>
-
+                <h1>CELEBRANDO O BRASIL & SUAS BELEZAS</h1>
             </div>
         )
     }
--- src/components/HomepageTitle/HomepageTitle.test.js
+++ src/components/HomepageTitle/HomepageTitle.test.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import {shallow} from "enzyme";
+import {shallow, render} from "enzyme";
 
 import HomepageTitle from './HomepageTitle.js'
 
 
 describe('The Homepage Title Component', () => {
     it('renders OK', () => {
-        const homepageTitleComponent = shallow(<HomepageTitle/>)
+        const homepageTitleComponent = shallow(<HomepageTitle />)
         expect(homepageTitleComponent.exists()).toEqual(true)
+    })
+
+    it('welcomes visitors with a friendly title', () => {
+        const homepageTitleComponent = render(<HomepageTitle />)
+        const hompageTitleText = homepageTitleComponent.text()
+        expect(hompageTitleText).toEqual('CELEBRANDO O BRASIL & SUAS BELEZAS')
     })
 })
