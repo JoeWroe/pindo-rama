8d3b3c985e6992e372209151fe240479ab3728f6
Joe Wroe <joe_wroe@icloud.com>
2018-11-27 20:16:14 +0000

    Add brand description to HomepageContent
    Joe

--- src/components/HomepageContent/HomepageContent.js
+++ src/components/HomepageContent/HomepageContent.js
@@ -5,6 +5,16 @@ class HomepageContent extends Component {
     render() {
         return (
             <div className='homepageContent'>
+                <p>
+                    Pindorama, palavra derivada do Tupi Guarani, {"\n"}
+                    que significa “Terra livre dos males” {"\n"}
+                    e/ou “Terra das Palmeiras”. {"\n"}
+                    Assim era como os povos indígenas {"\n"}
+                    chamavam o Brasil antesdo descobrimento. {"\n"}
+                    Assim é como nós chamamos a marca onde {"\n"}
+                    queremos trazer um pouco da memória brasileira {"\n"}
+                    para aqueles que vivem longe de casa.
+                </p>
             </div>
         )
     }
--- src/components/HomepageContent/HomepageContent.test.js
+++ src/components/HomepageContent/HomepageContent.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {shallow} from "enzyme";
+import {render, shallow} from "enzyme";
 
 import HomepageContent from './HomepageContent.js'
 
@@ -8,5 +8,20 @@ describe('The Homepage Content Component', () => {
     it('renders OK', () => {
         const homepageContentComponent = shallow(<HomepageContent />)
         expect(homepageContentComponent.exists()).toEqual(true)
+    })
+
+    it('displays the brand description', () => {
+        const homepageContentComponent = render(<HomepageContent />)
+        const hompageContentText = homepageContentComponent.text()
+        expect(hompageContentText).toEqual(
+            'Pindorama, palavra derivada do Tupi Guarani, \n' +
+            'que significa “Terra livre dos males” \n' +
+            'e/ou “Terra das Palmeiras”. \n' +
+            'Assim era como os povos indígenas \n' +
+            'chamavam o Brasil antesdo descobrimento. \n' +
+            'Assim é como nós chamamos a marca onde \n' +
+            'queremos trazer um pouco da memória brasileira \n' +
+            'para aqueles que vivem longe de casa.'
+        )
     })
 })
