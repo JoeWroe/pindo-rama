386ba25b8361575eaf96f704a35a17ddce3d302a
Joe Wroe <joe_wroe@icloud.com>
2018-11-29 16:25:51 +0000

    Style social media logos
    Joe

--- src/components/SocialMediaButton/SocialMediaButton.js
+++ src/components/SocialMediaButton/SocialMediaButton.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import './SocialMediaButton.scss'
 import { socialMediaAssets } from '../../content/assets'
 
 const SocialMediaButton = (props) => <img src={ socialMediaAssets[props.pathToIcon]} alt={props.altText}/>
--- src/components/SocialMediaButtons/SocialMediaButtons.js
+++ src/components/SocialMediaButtons/SocialMediaButtons.js
@@ -1,14 +1,18 @@
 import React, {Component} from 'react';
+import './SocialMediaButtons.scss'
+
 import SocialMediaButton from "../SocialMediaButton/SocialMediaButton";
+
+
 
 class SocialMediaButtons extends Component {
 
     render() {
         return (
             <div className='socialMediaButtons'>
-                <SocialMediaButton pathToIcon={'etsyIcon'} altText='Etsy' />
                 <SocialMediaButton pathToIcon={'facebookIcon'} altText='Facebook' />
                 <SocialMediaButton pathToIcon={'instagramIcon'} altText='Instagram' />
+                <SocialMediaButton pathToIcon={'etsyIcon'} altText='Etsy' />
             </div>
         )
     }
