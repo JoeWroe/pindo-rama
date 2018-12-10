f108323c40498fab2d997a1aad9b3a86498d08d9
Joe Wroe <joe_wroe@icloud.com>
2018-12-10 17:29:54 +0000

    Add browser router for routering
    Joe

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import { BrowserRouter, Route } from "react-router-dom";
+
 import Homepage from "./components/Homepage/Homepage";
 
 const App = () => {
     return (
-        <div className="App">
-            <Homepage/>
-        </div>
+        <BrowserRouter>
+            <div className="App">
+                <Route exact path='/' render={() => (<Homepage/>)} />
+            </div>
+        </BrowserRouter>
     )
 }
 
