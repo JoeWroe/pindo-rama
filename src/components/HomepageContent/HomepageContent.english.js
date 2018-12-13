65e48f2c196f32367cba6f7afc98e4be02fde81b
Joe Wroe <joe_wroe@icloud.com>
2018-12-13 19:04:29 +0000

    Add 'show' class to content component when it mounts
    ∆ Also added its removal when it unmounts.
    
    ∆ This will be used to trigger the CSS fade transition.
    
    Joe

--- src/components/HomepageContent/HomepageContent.english.js
+++ src/components/HomepageContent/HomepageContent.english.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './HomepageContent.scss'
 
-const EnglishHomepageContent = () => {
-    return (
-        <div className='homepageContent'>
-            <p>Pindorama, word derived from the Guarani Tupi, </p>
-            <p>which means “Land free from evil” </p>
-            <p>and / or “Terra das Palmeiras”. </p>
-            <p>This was how the indigenous peoples </p>
-            <p>called Brazil before its discovery. </p>
-            <br />
-            <p>This is how we call the brand where we </p>
-            <p>want to bring some of the Brazilian culture, </p>
-            <p>beauty and history. </p>
-        </div>
-    )
+class EnglishHomepageContent extends Component {
+    componentClasses = ['homepageContent']
+
+    componentWillMount = () => {
+        this.componentClasses.push('show')
+    }
+
+    componentWillUnmount() {
+        this.componentClasses.splice(this.componentClasses.indexOf('show'), 1)
+    }
+
+    render () {
+        return (
+            <div className={this.componentClasses.join(' ')}>
+                <p>Pindorama, word derived from the Guarani Tupi, </p>
+                <p>which means “Land free from evil” </p>
+                <p>and / or “Terra das Palmeiras”. </p>
+                <p>This was how the indigenous peoples </p>
+                <p>called Brazil before its discovery. </p>
+                <br/>
+                <p>This is how we call the brand where we </p>
+                <p>want to bring some of the Brazilian culture, </p>
+                <p>beauty and history. </p>
+            </div>
+        )
+    }
 }
 
 export default EnglishHomepageContent;
