6afde0d9e650d22f98dbaf6c10e895f647155745
Joe Wroe <joe_wroe@icloud.com>
2018-12-16 00:29:09 +0000

    Add fade in on homepage content appearance
    Joe

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react'
+import {CSSTransition} from 'react-transition-group'
+
+import './Homepage.scss'
 
 import BrazilianHomepageContent from "../HomepageContent/HomepageContent.brazilian";
 import BrazilianHomepageTitle from "../HomepageTitle/HomepageTitle.brazilian";
@@ -18,16 +21,18 @@ class Homepage extends Component {
 
     render() {
         return (
-            <div className="homepage">
-                <HomepageLogo/>
-                {(this.state.language === 'brazilian') ?
-                    <BrazilianHomepageTitle /> :
-                    <EnglishHomepageTitle />}
-                {(this.state.language === 'brazilian') ?
-                    <BrazilianHomepageContent /> :
-                    <EnglishHomepageContent />}
-                <HomepageButtons setLanguage={this.setLanguage}/>
-            </div>
+            <CSSTransition in={true} appear={true} timeout={1500} classNames='fade'>
+                <div className="homepage">
+                    <HomepageLogo/>
+                    {(this.state.language === 'brazilian') ?
+                        <BrazilianHomepageTitle/> :
+                        <EnglishHomepageTitle/>}
+                    {(this.state.language === 'brazilian') ?
+                        <BrazilianHomepageContent/> :
+                        <EnglishHomepageContent/>}
+                    <HomepageButtons setLanguage={this.setLanguage}/>
+                </div>
+            </CSSTransition>
         )
     }
 }
