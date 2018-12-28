38556d9ea01f6aa6cfdbdc0e7fefe92c20defaf0
Joe Wroe <joe_wroe@icloud.com>
2018-12-28 23:51:50 +0000

    Rename tf_vars to not use shorthand
    Joe

--- infra/provider.tf
+++ infra/provider.tf
@@ -1,8 +1,8 @@
-variable "do_token" {}
-variable "pub_key" {}
-variable "pvt_key" {}
+variable "digitalocean_token" {}
+variable "public_key" {}
+variable "private_key" {}
 variable "ssh_fingerprint" {}
 
 provider "digitalocean" {
-  token = "${var.do_token}"
+  token = "${var.digitalocean_token}"
 }
