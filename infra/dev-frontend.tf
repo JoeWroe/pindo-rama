316fa5a425214e89f8154d2a861fb9868f3f0384
Joe Wroe <joe_wroe@icloud.com>
2018-12-29 12:15:42 +0000

    Remove surplus NGINX configuration
    Joe

--- infra/dev-frontend.tf
+++ infra/dev-frontend.tf
@@ -3,24 +3,4 @@ resource "digitalocean_droplet" "dev-frontend" {
   name = "dev-frontend"
   region = "lon1"
   size = "512mb"
-  private_networking = true
-  ssh_keys = [
-    "${var.ssh_fingerprint}"
-  ]
-
-  connection {
-    user = "root"
-    type = "ssh"
-    private_key = "${file(var.private_key)}"
-    timeout = "2m"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "export PATH=$PATH:/usr/bin",
-      # install nginx
-      "sudo apt-get update",
-      "sudo apt-get -y install nginx"
-    ]
-  }
 }
