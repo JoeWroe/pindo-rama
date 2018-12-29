0c210878ee239c6018530650b4ab7987e8770ec3
Joe Wroe <joe_wroe@icloud.com>
2018-12-29 12:29:07 +0000

    Add domian to dev-frontend droplet
    Joe

--- infra/dev-frontend.tf
+++ infra/dev-frontend.tf
@@ -4,3 +4,8 @@ resource "digitalocean_droplet" "dev-frontend" {
   region = "lon1"
   size = "512mb"
 }
+
+resource "digitalocean_domain" "pindo-rama" {
+  ip_address = "${digitalocean_droplet.dev-frontend.ipv4_address}"
+  name = "pindo-rama.art"
+}
